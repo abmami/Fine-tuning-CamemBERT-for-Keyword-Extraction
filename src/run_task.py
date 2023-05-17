fc895236357d58be8c8f901b9002b625e80786df
abmami <abdessalemmami@gmail.com>
2023-05-17 16:15:51 +0100

    :art: Refactored run_task code

--- src/run_task.py
+++ src/run_task.py
@@ -3,102 +3,57 @@ import lightning.pytorch as pl
 import numpy as np
 import random
 import warnings
-warnings.filterwarnings("ignore")
+import argparse
+import time 
+from sentence_similiarity import dataloader as ss_dataloader
+from sentence_similiarity import model as ss_model
+from keyword_extraction import dataloader as ke_dataloader
+from keyword_extraction import model as ke_model
 
-# Set the random seed for reproducibility
+warnings.filterwarnings("ignore")
 torch.manual_seed(42)
 np.random.seed(42)
 random.seed(42)
 pl.trainer.seed_everything(42)
 
-from sentence_similiarity import dataloader as ss_dataloader
-from sentence_similiarity import model as ss_model
+from utils import models
 
-from keyword_extraction import dataloader as ke_dataloader
-from keyword_extraction import model as ke_model
+def run_task(task_name, from_scratch):
+    model_path = models["models_folder"] + task_name + "/"
+    batch_size = models[task_name]["batch_size"]
+    max_length = models[task_name]["max_length"]
 
-def run_task(task_name, batch_size, epochs, lr, weight_decay, from_scratch=False, quantize=False):
     if task_name == "ss":
-        
-        train_dataloader, val_dataloader, test_dataloader = ss_dataloader.load_ss(batch_size=batch_size)
-        model, trainer = ss_model.run_trainer(train_dataloader, val_dataloader, test_dataloader, epochs, lr, weight_decay, from_scratch)
-        ## save model
-        model.save_pretrained("../models/ss")
-
-        ## save trainer 
-        trainer.save_checkpoint("../models/ss/trainer.ckpt")
-
-        ## torch save pt
-        torch.save(model.state_dict(), "../models/ss/model.pt")
+        train_dataloader, val_dataloader, test_dataloader = ss_dataloader.load_ss(batch_size=batch_size, max_length=max_length)
+        model, trainer = ss_model.run_trainer(train_dataloader, val_dataloader, test_dataloader,from_scratch)
 
     elif task_name == "ke":
-        train_dataloader, val_dataloader, test_dataloader = ke_dataloader.load_ke(batch_size=4)
-        model, trainer = ke_model.run_trainer(train_dataloader, val_dataloader, test_dataloader, epochs=3, lr=2e-5, max_length=256)
-        ## save model
-        model.save_pretrained("../models/ke")
-
-        ## save trainer 
-        trainer.save_checkpoint("../models/ke/trainer.ckpt")
-
-        ## torch save pt
-        torch.save(model.state_dict(), "../models/ke/model.pt")
+        train_dataloader, val_dataloader, test_dataloader = ke_dataloader.load_ke(batch_size=batch_size, max_length=max_length)
+        model, trainer = ke_model.run_trainer(train_dataloader, val_dataloader, test_dataloader,from_scratch)
+        
     else:
         raise ValueError("Unknown task name")
     
+    model.save_pretrained(model_path)
+    trainer.save_checkpoint(model_path + "trainer.ckpt")
+    torch.save(model.state_dict(), model_path + "model.pt")
+    
 
-### main with arguments
-
-import argparse
-import time 
 def main():
-    # task_name, batch_size, epochs, lr, weight_decay
-
-    # task_name, batch_size, epochs, lr, max_length
 
     parser = argparse.ArgumentParser(description='Fine-tune a model on a task')
     parser.add_argument('--task', type=str, default="ss",help='task name')
-    parser.add_argument('--batch_size', type=int, default=16, help='batch size')
-    parser.add_argument('--epochs', type=int, default=1, help='epochs')
-    parser.add_argument('--lr', type=float, default=3e-5, help='learning rate')
-    parser.add_argument('--wd', type=float, default=0., help='weight decay')
-    parser.add_argument('--from_scratch', type=bool, default=False, help='from scratch')
-    parser.add_argument('--quantize', type=bool, default=False, help='quantize')
-
     args = parser.parse_args()
     task_name = args.task
-    batch_size = args.batch_size
-    epochs = args.epochs
-    lr = args.lr
-    wd = args.wd
-    from_scratch = args.from_scratch
-    quantize = args.quantize
-
     time_start = time.time()
-    if task_name == "ss":
-        run_task("ss", batch_size, epochs, lr, wd, from_scratch, quantize)
-        
-    elif task_name == "ke":
-        run_task("ke", batch_size, epochs, lr, wd, from_scratch,quantize)
-    else:
-        raise ValueError("Unknown task name")
+    run_task(task_name, from_scratch=False)
     time_end = time.time() 
+
     print('Task completed in {} seconds'.format(time_end - time_start))
     
 if __name__ == "__main__":
     main()
 
-
-### To run the script:
-
-# python run_task.py --task ss --batch_size 16 --epochs 1 --lr 3e-5 --wd 0. --from_scratch False --quantize False
-
-# --task: task name, ss for sentence similarity and ke for keyphrase extraction
-# --batch_size: batch size
-# --epochs: number of epochs
-# --lr: learning rate
-# --wd: weight decay
-# --from_scratch: whether to train from scratch or not
-# --quantize: whether to quantize the model or not
 
 
 
--- src/utils.py
+++ src/utils.py
@@ -1,24 +1,41 @@
-import warnings
-warnings.filterwarnings("ignore")
 
-
-
-data_folder = "../data/"
 
 data = {
+    "data_folder":"../data/",
     "PAWS-C-FR":{
     "task":"ss",
-    "train": data_folder + "PAWS-C-FR/translated_train.tsv",
-    "dev": data_folder + "PAWS-C-FR/dev_2k.tsv",
-    "test": data_folder + "PAWS-C-FR/test_2k.tsv"
+    "train":"PAWS-C-FR/translated_train.tsv",
+    "dev": "PAWS-C-FR/dev_2k.tsv",
+    "test": "PAWS-C-FR/test_2k.tsv"
     },
     "KEYS-DATASET":{
     "task":"ke",
-    "train": data_folder + "KEYS-DATASET/train.csv",
-    "dev": data_folder + "KEYS-DATASET/dev.csv",
-    "test": data_folder + "KEYS-DATASET/test.csv"
+    "train": "KEYS-DATASET/train.csv",
+    "dev": "KEYS-DATASET/dev.csv",
+    "test": "KEYS-DATASET/test.csv"
     }
 }
 
+models = { 
+    "models_folder":"../models/",
+    "ss":{
+        "model_name":"camembert-base",
+        "epochs":1,
+        "lr":  3e-5,
+        "batch_size": 16,
+        "accumulate_grad_batches":8,
+        "max_length": 128,      
+        "weight_decay": 0.,
+    },
 
-models_folder = "../models/"
+    "ke": {
+        "model_name":"camembert-base",
+        "epochs":3,
+        "lr":  2e-5,
+        "batch_size": 4,
+        "accumulate_grad_batches":8,
+        "max_length": 256,
+        "eps": 1e-08, 
+        "betas": (0.9, 0.999),
+    }
+}
